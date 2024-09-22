c6c2318f27543ba35f0f0b210cd1d98a3b9d1990
pieroangulocubas <pieroangulocubas.17@gmail.com>
2024-09-22 12:12:20 -0500

    feat(auth): add authentication with Clerk

--- app/documents/[id]/page.tsx
+++ app/documents/[id]/page.tsx
@@ -1,8 +1,19 @@
 import { Editor } from "@/components/editor/Editor"
+import Header from "@/components/Header"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
 
 const Document = () => {
   return (
     <div>
+      <Header>
+          <p>Hola mundo</p>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+      </Header>
       <Editor/>
     </div>
   )
